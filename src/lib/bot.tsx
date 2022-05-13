4be33b88dcba58b4a29f243a21f89132b64109d5
FOREVEREALIZE <60579848+FOREVEREALIZE@users.noreply.github.com>
2022-05-13 09:55:55 +0200

    Add a bit of word handling

--- src/lib/bot.tsx
+++ src/lib/bot.tsx
@@ -1,23 +1,31 @@
 import { WORDS } from '../constants/wordlist'
-import { findFirstUnusedReveal } from './words'
+import { solution as sol } from './words'
+import { getStatuses } from './statuses'
 
 function getRandomInt(max: number) {
-    return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max)
 }
 
 function getGuess(guesses: string[]) {
-    const valid = getValidWords(guesses)
+    const valid = getValidWords(guesses, sol)
     return(valid[getRandomInt(valid.length)].toUpperCase())
 }
 
-function getValidWords(guesses: string[]): string[] {
-    var validWords: string[] = [];
-    for (const word in WORDS) {
-        if (!findFirstUnusedReveal(word, guesses)) {
-            validWords.push(word)
+function getValidWords(guesses: string[], solution: string): string[] {
+    var validLetters: string[] = [ 'abcdefghijklmnopqrstuvwxyz', 'abcdefghijklmnopqrstuvwxyz', 'abcdefghijklmnopqrstuvwxyz', 'abcdefghijklmnopqrstuvwxyz', 'abcdefghijklmnopqrstuvwxyz' ]
+    for ( guess in guesses ) {
+        var i = 0
+        var status = getStatuses(solution, guesses)
+        for ( letter in guess ) {
+            if ( status[letter] === 'absent' ) {
+                for ( column in validLetters ) {
+                    validLetters[validLetters.indexOf(column)] = column.split('').splice(column.split('').indexOf(letter), 1).join('')
+                }
+            }
+            validLetters[i] = validLetters[i].split('').splice(status[letter])
+            i++
         }
     }
-    return validWords;
 }
 
 export default {
